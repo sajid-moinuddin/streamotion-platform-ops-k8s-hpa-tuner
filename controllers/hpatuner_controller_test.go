1cc05a5ba7b91000abb8e3033ebab510d1e44160
Sajid Moinuddin <sajid.moinuddin@foxsports.com.au>
2020-07-24 09:20:06 +1000

    updated tests

--- controllers/hpatuner_controller_test.go
+++ controllers/hpatuner_controller_test.go
@@ -3,18 +3,39 @@ package controllers
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"log"
+
 	// "hpa-tuner/controllers"
 )
 
 var _ = Describe("HpatunerController", func() {
+	logger := log.New(GinkgoWriter, "INFO: ", log.Lshortfile)
+	//ctx := context.Background()
+
+	BeforeEach(func() {
+		// failed test runs that don't clean up leave resources behind.
+	})
+
+	AfterEach(func() {
+
+	})
+
 	Context("test some stuff", func() {
 
-		It("Should correctly identify initStartedEvent", func() {
+		It("Should Be Able to create HpaTuner CRDs2", func() {
+			logger.Println("----------------start test-----------")
 			isInteresting := true
-
-			print(k8sClient)
-
 			Expect(isInteresting).To(BeTrue())
+			Expect(!isInteresting).To(BeFalse())
+
+		})
+
+		It("Should Be Able to create HpaTuner CRDs", func() {
+			logger.Println("----------------start test-----------")
+			isInteresting := true
+			Expect(isInteresting).To(BeTrue())
+			Expect(!isInteresting).To(BeFalse())
+
 		})
 	})
 
