9e3800cef145d0aafb714c8c51d76a7d7dd54b27
federico.nervegna <federico.nervegna@foxsports.com.au>
2020-10-01 12:29:35 +1000

    fix jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -40,41 +40,41 @@ pipeline {
           sh "export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml"
           sh "export VERSION=latest && skaffold build -f skaffold.yaml"
           script {
-            def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
+            def buildVersion = readFile "${env.WORKSPACE}/VERSION"
             currentBuild.description = "${DOCKER_REGISTRY}/streamotion-platform-ops-k8s-hpa-tuner:$buildVersion"
             currentBuild.displayName = "$buildVersion"
           }
         }
       }
     }
-  }
-  stage('Promote to Environments') {
-    when {
-      branch 'master'
-    }
-    steps {
-      container('streamotion-go') {
-        sh "mv charts/helm-release  charts/$APP_NAME"
-        dir("charts/$APP_NAME") {
-          sh "jx step changelog --generate-yaml=false --version v\$(cat ../../VERSION)"
 
-          sh "make release"
-          // promote through all 'Staging' promotion Environments
-          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=bedrock-staging --version \$(cat ../../VERSION)"
-//          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-staging --version \$(cat ../../VERSION)"
-//            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-staging --version \$(cat ../../VERSION)"
-//            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-staging --version \$(cat ../../VERSION)"
+    stage('Promote to Environments') {
+      when {
+        branch 'master'
+      }
+      steps {
+        container('streamotion-go') {
+          sh "mv charts/helm-release  charts/$APP_NAME"
+          dir("charts/$APP_NAME") {
+            sh "jx step changelog --generate-yaml=false --version v\$(cat ../../VERSION)"
 
-          // promote through all 'Production' promotion Environments
-//          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-production --version \$(cat ../../VERSION)"
-          // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-production --version \$(cat ../../VERSION)"
-          // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-production --version \$(cat ../../VERSION)"
+            sh "make release"
+            // promote through all 'Staging' promotion Environments
+            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=bedrock-staging --version \$(cat ../../VERSION)"
+            //          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-staging --version \$(cat ../../VERSION)"
+            //            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-staging --version \$(cat ../../VERSION)"
+            //            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-staging --version \$(cat ../../VERSION)"
 
+            // promote through all 'Production' promotion Environments
+            //          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-production --version \$(cat ../../VERSION)"
+            // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-production --version \$(cat ../../VERSION)"
+            // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-production --version \$(cat ../../VERSION)"
+
+          }
         }
       }
     }
   }
-
   post {
     always {
       cleanWs()
