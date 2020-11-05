b8b060154e8560c66963b18a20cd99daebd97f9c
Sajid Moinuddin <sajid.moinuddin@kayosports.com.au>
2020-11-05 13:52:39 +1100

    Release

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -82,7 +82,7 @@ pipeline {
             }
         }
 
-        stage('Promote to Environments') {
+        stage('release') {
             when {
                 branch 'master'
             }
@@ -94,17 +94,6 @@ pipeline {
                         sh "jx step changelog --generate-yaml=false --version v\$(cat ../../VERSION)"
 
                         sh "make release"
-                        // promote through all 'Staging' promotion Environments
-                        sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=platform-bedrock --version \$(cat ../../VERSION)"
-                        //          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-staging --version \$(cat ../../VERSION)"
-                        //            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-staging --version \$(cat ../../VERSION)"
-                        //            sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-staging --version \$(cat ../../VERSION)"
-
-                        // promote through all 'Production' promotion Environments
-                        //          sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=commerce-production --version \$(cat ../../VERSION)"
-                        // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=content-production --version \$(cat ../../VERSION)"
-                        // sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=streamtech-production --version \$(cat ../../VERSION)"
-
                     }
                 }
             }
